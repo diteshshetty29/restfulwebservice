1087932a939e59a0537116ce5d90ce119b1fdb01
ditesh shetty <diteshshetty@gmail.com>
2015-07-13 16:26:15 +0530

    finished example 1

--- src/com/ditesh/restful/services/V1_Status.java
+++ src/com/ditesh/restful/services/V1_Status.java
@@ -5,9 +5,17 @@ import javax.ws.rs.core.MediaType;
 @Path("/v1/status")
 public class V1_Status {
 
+	private static final String api_version = "00.01.00";
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public String returnTitle(){
 		return "<p>Java Rest Web Services</p>";
+	}
+	
+	@Path("/version")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnVersion(){
+		return "<p>Version:</p>"+api_version;
 	}
 }
